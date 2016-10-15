bcde8b5bd5bb14ea8279db28d25850047a2d6279
hugomorelli <hugomorelli2@gmail.com>
2016-10-15 16:40:38 -0400

    first commit

---
+++ Times Table/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Times Table
+//
+//  Created by Hugo Morelli on 9/28/16.
+//  Copyright © 2016 Hugo Morelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
+    //MARK: Properties
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var table: UITableView!
+    //MARK: Actions
+    @IBAction func sliderChanged(_ sender: AnyObject) {
+        table.reloadData()
+        
+    }
+    
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 50
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = String((Int(slider.value * 20)) * (indexPath.row + 1))
+        return cell
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
